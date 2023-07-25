6f4687fa853c9cd9c4a7ecb1017de63fb2d8c4dd
Alexandre Pateis da Rocha <alepateis@users.noreply.github.com>
2023-07-25 15:03:47 -0300

    melhorando o css

--- src/components/Details/BreedCard.js
+++ src/components/Details/BreedCard.js
@@ -5,22 +5,20 @@ import tw from 'twrnc';
 export default function BreedCard({ breed }) {
   return (
     <View style={tw`p-4 bg-white rounded-md shadow-md flex-1`}>
-      <Text style={tw`font-bold text-lg mb-4 text-center`}>
+      <Text style={tw`font-bold text-2xl mb-4 text-center text-gray-800`}>
         Dog Information
       </Text>
       <FlatList
         data={breed}
         renderItem={({ item }) => (
-          <View
-            key={breed.id}
-            style={tw`my-4 border rounded-md p-4 bg-gray-100`}
-          >
-            <Text style={tw`text-base font-bold mb-2`}>Name: {item.name}</Text>
-            <Text style={tw`text-base`}>Bred for: {item.bred_for}</Text>
-            <Text style={tw`text-base`}>Temperament: {item.temperament}</Text>
-            <Text style={tw`text-base`}>Life span: {item.life_span}</Text>
+          <View style={tw`my-4 border rounded-md p-4 bg-gray-100 shadow-md`}>
+            <Text style={tw`text-xl font-bold mb-2 text-gray-800`}>Name: {item.name}</Text>
+            <Text style={tw`text-base text-gray-600`}>Bred for: {item.bred_for}</Text>
+            <Text style={tw`text-base text-gray-600`}>Temperament: {item.temperament}</Text>
+            <Text style={tw`text-base text-gray-600`}>Life span: {item.life_span}</Text>
           </View>
         )}
+        keyExtractor={(item) => item.id.toString()} // Assuming "id" is a unique identifier for each item
       />
     </View>
   );
--- src/screens/FurtherDetails.js
+++ src/screens/FurtherDetails.js
@@ -6,32 +6,33 @@ export default function FurtherDetails({ route }) {
   const { dog } = route.params;
   return (
     <View style={tw`p-4 bg-white rounded-md shadow-md flex-1`}>
-      <Text style={tw`font-bold text-lg mb-4 text-center`}>
-        Further Information
-      </Text>
-      <FlatList
-        data={dog.breeds}
-        renderItem={({ item }) => (
-          <View style={tw`my-4 border rounded-md p-4 bg-gray-100`}>
-            <Text style={tw`text-base font-bold mb-2`}>
-              Imperial weight: {item.weight.imperial}
-            </Text>
-            <Text style={tw`text-base`}>
-              Metric weight: {item.weight.metric}
-            </Text>
-            <Text style={tw`text-base`}>
-              Imperial height: {item.height.imperial}
-            </Text>
-            <Text style={tw`text-base`}>
-              Metric height: {item.height.metric}
-            </Text>
-            <Text style={tw`text-base`}>
-              Reference image ID: {item.reference_image_id}
-            </Text>
-          </View>
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </View>
+  <Text style={tw`font-bold text-2xl mb-4 text-center text-gray-800`}>
+    Further Information
+  </Text>
+  <FlatList
+    data={dog.breeds}
+    renderItem={({ item }) => (
+      <View style={tw`my-4 border rounded-md p-4 bg-gray-100 shadow-md`}>
+        <Text style={tw`text-lg font-bold mb-2 text-gray-800`}>
+          Imperial weight: {item.weight.imperial}
+        </Text>
+        <Text style={tw`text-lg text-gray-600`}>
+          Metric weight: {item.weight.metric}
+        </Text>
+        <Text style={tw`text-lg font-bold mb-2 text-gray-800`}>
+          Imperial height: {item.height.imperial}
+        </Text>
+        <Text style={tw`text-lg text-gray-600`}>
+          Metric height: {item.height.metric}
+        </Text>
+        <Text style={tw`text-lg text-gray-600`}>
+          Reference image ID: {item.reference_image_id}
+        </Text>
+      </View>
+    )}
+    keyExtractor={(item, index) => index.toString()}
+  />
+</View>
+
   );
 }
